9185c396c10412eb8e8dade21ea58df8777de696
PassionZale <372470745@qq.com>
2015-07-04 11:48:33 +0800

    修改js,完善readme.md

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # control_table
 jquery+bootstrap 控制table中的th在浏览器滚动条滚动时,一直显示在tbody的上方
+
+这只是一个页面的content部分,header & footer并未添加
+
+1.实际页面中,需要加上header的高度;
+
+2.通过offset()获取th这一行所处的位置,返回obj{top:(num),left:(num)};
+
+3.监听DOM对象滚动scroll事件,并获取滚动的top距离:scrollTop;
+
+4.top = scrollTop + offset.top,即是th应该移动到的位置;
+
+5.调用offset({top:top}) 将4步骤中的值赋值给其top属性;
+
+6.设置其position为'fixed' || 'absolute',让其脱离标准文档流,即可让其随滚动条滚动而一直固定在某个位置;
+
+7.由于脱离文档流,且该table为bootstrap的table控件,width样式消失,最后调用get_width();
+
+8.将tbody中的一行tr,循环其td,获取其innerWidth(),并赋值给th中的td,即可.
--- main.js
+++ main.js
@@ -2,14 +2,16 @@
  * Created by zl on 2015/7/3.
  */
 
-$(function(){
+$(function () {
     var $node = $('#tablewrapper table tr:eq(0)');
-    var str = '';
-    var get_width = function(){
-        for(var i = 0; i <$('#tablewrapper table tr:eq(1) td').length; i++ ){
-            str = $($('#tablewrapper table tr:eq(1) td')[i]).html();
-            if( str == '') continue;
-            $('#tablewrapper table tr:eq(0) th:eq('+i+')').css('width',$('#tablewrapper table tr:eq(1) td:eq('+i+')').innerWidth());
+    var $td_node_second = $('#tablewrapper table tr:eq(1) td');
+    var get_width = function () {
+        for (var i = 0, length = $td_node_second.length; i < length; i++) {
+            var str = $($td_node_second[i]).html();
+            //tbody中如果有的tr为空,由于bootstrap样式的原因,为空的tr如果设置高度,则会导致表头的文字无法正确排版
+            if (str == '') continue;
+            //innerWidth可以包含padding,如果没有padding可以直接使用width
+            $('#tablewrapper table tr:eq(0) th:eq(' + i + ')').css('width', $('#tablewrapper table tr:eq(1) td:eq(' + i + ')').innerWidth());
         }
     }
     //var header_height = $('header').height();//header的高度
@@ -18,13 +20,13 @@ $(function(){
     var tr_offset = $tr.offset();
     //var top = header_height;
     //console.log(top);
-    $(window.document).scroll(function(){
+    $(window.document).scroll(function () {
         var scrollTop = $(document).scrollTop();
         var top = offset.top + scrollTop;
-        $node.css({'position':'fixed','z-index':'200'}).offset({top:top});
+        $node.css({'position': 'fixed', 'z-index': '200'}).offset({top: top});
         var mid_val = tr_offset.top + scrollTop;
-        if(tr_offset.top == mid_val){
-            $node.css('position','');
+        if (tr_offset.top == mid_val) {//当滚动到原位置时,恢复$node的样式.
+            $node.css('position', '');
         }
         get_width();
     });
